98bde6bcfeb68183c2ef2b2c99e488bac5a083a1
Cleiton Schulz <cleiton@schulz.eng.br>
2022-09-26 17:58:26 -0300

    nginx hello

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,11 @@ ADD amzn2-container-raw-2.0.20220805.0-x86_64.tar.xz /
 
 ARG IMAGE_CONTAINERBUILD1 IMAGE_CONTAINERBUILD1_URL IMAGE_CONTAINERBUILD1_VERSAO IMAGE_CONTAINERBUILD1_CERT IMAGE_CONTAINERBUILD1_CERTKEY IMAGE_CONTAINERBUILD1_CERTDIR
 
-RUN yum update -y
+#RUN yum update -y
 
-RUN yum install net-tools procps -y
+RUN amazon-linux-extras enable nginx1 -y
 
-RUN amazon-linux-extras install nginx1 -y
+RUN yum install net-tools procps nginx -y
 
 copy ajustes-site.sh /bin/
 
--- ajustes-site.sh
+++ ajustes-site.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-/usr/sbin/nginx
 
 mv /var/www/site1 /var/www/$IMAGE_CONTAINERBUILD1
 
 mv /etc/nginx/conf.d/site1.conf /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
 
-sed -i "s,sed_site,$IMAGE_CONTAINERBUILD1_URL,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
+sed -i "s,sed_site_url,$IMAGE_CONTAINERBUILD1_URL,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
+sed -i "s,sed_site_dir,$IMAGE_CONTAINERBUILD1,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
+
+/usr/sbin/nginx
 
 exec /usr/sbin/init
+
