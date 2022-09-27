548f9ff6b810ea505830a3c51861285260a3a1a6
Cleiton Schulz <cleiton@schulz.eng.br>
2022-09-27 09:01:33 -0300

    commit

--- ajustes-site.sh
+++ ajustes-site.sh
@@ -6,6 +6,7 @@ mv /var/www/site1 /var/www/$IMAGE_CONTAINERBUILD1
 
 mv /etc/nginx/conf.d/site1.conf /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
 
-sed -i "s,sed_site,$IMAGE_CONTAINERBUILD1_URL,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
+sed -i "s,sed_site_url,$IMAGE_CONTAINERBUILD1_URL,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
+sed -i "s,sed_site_dir,$IMAGE_CONTAINERBUILD1,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
 
 exec /usr/sbin/init
