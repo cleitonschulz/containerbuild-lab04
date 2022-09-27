e488fab1a929f5f390293ca871aea46539a09656
Cleiton Schulz <cleiton@schulz.eng.br>
2022-09-27 16:10:47 -0300

    dale

--- ajustes/ajustes-site.sh
+++ ajustes/ajustes-site.sh
@@ -6,6 +6,11 @@ mv /etc/nginx/conf.d/site1.conf /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
 
 sed -i "s,sed_site_url,$IMAGE_CONTAINERBUILD1_URL,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
 sed -i "s,sed_site_dir,$IMAGE_CONTAINERBUILD1,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
+if [ $IMAGE_CONTAINERBUILD_DEBUG = "SIM" ]
+then
+	sed -i "s,#DEBUGSED,error_log /var/log/nginx/error.log debug;,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
+fi
+
 
 /usr/sbin/nginx
 
