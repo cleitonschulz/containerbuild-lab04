d9cf61a99fd9a2dd44d731f1d17cd18dc41fcac2
Cleiton Schulz <cleiton@schulz.eng.br>
2022-09-29 08:37:09 -0300

    dale2

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 FROM scratch
 ADD amzn2-container-raw-2.0.20220805.0-x86_64.tar.xz /
 
-ARG IMAGE_CONTAINERBUILD1 IMAGE_CONTAINERBUILD1_URL IMAGE_CONTAINERBUILD1_VERSAO IMAGE_CONTAINERBUILD1_CERT IMAGE_CONTAINERBUILD1_CERTKEY IMAGE_CONTAINERBUILD1_CERTDIR IMAGE_CONTAINERBUILD_DEBUG
+ARG IMAGE_CONTAINERBUILD1 IMAGE_CONTAINERBUILD1_URL IMAGE_CONTAINERBUILD1_VERSAO IMAGE_CONTAINERBUILD1_CERT IMAGE_CONTAINERBUILD1_CERTKEY IMAGE_CONTAINERBUILD1_CERTDIR IMAGE_CONTAINERBUILD_DEBUG IMAGE_CONTAINERBUILD_SSL
 
 #RUN yum update -y
 
 RUN amazon-linux-extras enable nginx1 -y
 
 RUN yum install net-tools procps nginx -y
+
+RUN ln -s -f /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 
 copy ajustes/ajustes-site.sh /usr/bin/ajustes-site.sh
 
@@ -16,6 +18,9 @@ RUN mkdir -p /var/www/
 copy var/site1 /var/www/site1
 
 copy nginx-conf/site1.conf /etc/nginx/conf.d/site1.conf 
+copy nginx-conf/site1-redirect.conf /etc/nginx/conf.d/site1-redirect.conf 
+copy nginx-conf/site2.conf /etc/nginx/conf.d/site2.conf 
+copy nginx-conf/parametros.conf /etc/nginx/conf.d/parametros.conf
 
 run chmod 744 /bin/ajustes-site.sh
 
--- ajustes/ajustes-site.sh
+++ ajustes/ajustes-site.sh
@@ -6,11 +6,29 @@ mv /etc/nginx/conf.d/site1.conf /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
 
 sed -i "s,sed_site_url,$IMAGE_CONTAINERBUILD1_URL,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
 sed -i "s,sed_site_dir,$IMAGE_CONTAINERBUILD1,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
+
+if [ $IMAGE_CONTAINERBUILD_SSL = "SIM" ]
+then
+        mv /etc/nginx/conf.d/site1-redirect.conf /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
+	mv /etc/nginx/conf.d/site2.conf /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1-ssl.conf
+	sed -i "s,fullchain.pem,$IMAGE_CONTAINERBUILD1_CERT,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1-ssl.conf
+	sed -i "s,privkey.pem,$IMAGE_CONTAINERBUILD1_CERTKEY,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1-ssl.conf
+	sed -i "s,dhparam-2048.pem,$IMAGE_CONTAINERBUILD1_DHPARAM,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1-ssl.conf
+	sed -i "s,sed_site_url,$IMAGE_CONTAINERBUILD1_URL,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1-ssl.conf
+	sed -i "s,sed_site_dir,$IMAGE_CONTAINERBUILD1,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1-ssl.conf
+	if [ $IMAGE_CONTAINERBUILD_DEBUG = "SIM" ]
+	then
+		sed -i "s,#DEBUGSED,error_log /var/log/nginx/error.log debug;,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1-ssl.conf
+	fi
+else 
+	mv /etc/nginx/conf.d/site2.conf mv /etc/nginx/conf.d/site2-DESATIVADO
+        mv /etc/nginx/conf.d/site1-redirect.conf /etc/nginx/conf.d/site1-redirect-DESATIVADO
+fi
+
 if [ $IMAGE_CONTAINERBUILD_DEBUG = "SIM" ]
 then
 	sed -i "s,#DEBUGSED,error_log /var/log/nginx/error.log debug;,g" /etc/nginx/conf.d/$IMAGE_CONTAINERBUILD1.conf
 fi
-
 
 /usr/sbin/nginx
 
